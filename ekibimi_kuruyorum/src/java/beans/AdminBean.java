03f45fa597172a3c8a76dcc8afb7bfb91f796470
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2024-04-02 16:16:32 +0300

    .

--- ekibimi_kuruyorum/src/java/beans/AdminBean.java
+++ ekibimi_kuruyorum/src/java/beans/AdminBean.java
@@ -4,58 +4,22 @@
  */
 package beans;
 
-import dao.AdminDAO;
-import entity.Admin;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.SessionScoped;
 import java.io.Serializable;
-import java.util.List;
 
 /**
  *
- * @author admın
+ * @author Dell
  */
 @Named(value = "adminBean")
 @SessionScoped
 public class AdminBean implements Serializable {
-      private Admin entity;
-      private AdminDAO dao;
-      private List <Admin> list;
+
+    /**
+     * Creates a new instance of AdminBean
+     */
     public AdminBean() {
     }
-    
-    public void create(){
-        
-    }
-    public Admin getEntity() {
-        if(this.entity==null){
-            this.entity=new Admin();
-        }
-        return entity;
-    }
-
-    public void setEntity(Admin entity) {
-        this.entity = entity;
-    }
-
-    public AdminDAO getDao() {
-        if(this.dao==null){
-            this.dao=new AdminDAO();
-        }
-        return dao;
-    }
-
-    public void setDao(AdminDAO dao) {
-        this.dao = dao;
-    }
-
-    public List<Admin> getList() {
-        return list;
-    }
-
-    public void setList(List<Admin> list) {
-        this.list = list;
-    }
-     
     
 }
