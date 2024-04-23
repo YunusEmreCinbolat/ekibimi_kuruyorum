01a94e878117a3d277323ed916350b5b7f060f91
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2024-04-23 20:05:45 +0300

    add all function CRUD Sinif entity

---
+++ ekibimi_kuruyorum/src/java/dao/SinifDAO.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import entity.Admin;
+import entity.Sinif;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import util.Connector;
+
+/**
+ *
+ * @author Dell
+ */
+public class SinifDAO extends Connector {
+    
+       public void create(Sinif sinif) {
+        try {
+            Statement st = this.getConnect().createStatement();
+            st.executeUpdate("insert into siniflar (bolumid,sinifadi) values('" +sinif.getBolumid()+ "', '" +sinif.getSinifadi()+ "' )");
+        } catch (SQLException ex) {
+            Logger.getLogger(SinifDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+       
+        public void delete (int  id){
+        try {
+            Statement st = this.getConnect().createStatement();
+            st.executeUpdate("DELETE from siniflar where sinifid="+ id);
+        } catch (SQLException ex) {
+            Logger.getLogger(SinifDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+        
+         public List<Sinif> readList() {
+        List<Sinif> list = new ArrayList<>();
+
+        Statement st;
+        try {
+            st = this.getConnect().createStatement();
+            ResultSet rs = st.executeQuery("select * from siniflar");
+
+            while (rs.next()) {
+                list.add(new Sinif(rs.getLong("sinifid"),rs.getLong("bolumid"),rs.getInt("sinifadi")));
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(SinifDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+           public void update(Sinif sinif) {
+         try {
+            Statement st = this.getConnect().createStatement();
+            st.executeUpdate("update siniflar set sinifadi='"+sinif.getSinifadi()+"',  bolumid='"+sinif.getBolumid()+"' where sinifid="+sinif.getId());
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    
+}
---
+++ ekibimi_kuruyorum/src/java/entity/Sinif.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author Dell
+ */
+public class Sinif {
+    private Long id;
+    private Long bolumid;
+    private int sinifadi;
+
+    public Sinif() {
+    }
+
+    public Sinif(Long id, Long bolumid, int sinifadi) {
+        this.id = id;
+        this.bolumid = bolumid;
+        this.sinifadi = sinifadi;
+    }
+
+    public Sinif(Long bolumid, int sinifadi) {
+        this.bolumid = bolumid;
+        this.sinifadi = sinifadi;
+    }
+
+    public Sinif(int sinifadi) {
+        this.sinifadi = sinifadi;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getBolumid() {
+        return bolumid;
+    }
+
+    public void setBolumid(Long bolumid) {
+        this.bolumid = bolumid;
+    }
+
+    public int getSinifadi() {
+        return sinifadi;
+    }
+
+    public void setSinifadi(int sinifadi) {
+        this.sinifadi = sinifadi;
+    }
+    
+    
+    
+}
