c6b17ecc2def3ced50c7317390c4425d6f872378
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2024-04-23 23:34:26 +0300

    add Ogrenci entity

--- ekibimi_kuruyorum/src/java/entity/Ogrenci.java
+++ ekibimi_kuruyorum/src/java/entity/Ogrenci.java
@@ -10,4 +10,111 @@ package entity;
  */
 public class Ogrenci {
     
+    private Long id;
+    private String kullaniciadi;
+    private String email;
+    private String sifre;
+    private String ad;
+    private String soyad;
+    private String universite;
+    private Long sinifid;
+    private Long bolumid;
+
+    public Ogrenci(Long id, String kullaniciadi, String email, String sifre, String ad, String soyad, String universite, Long sinifid, Long bolumid) {
+        this.id = id;
+        this.kullaniciadi = kullaniciadi;
+        this.email = email;
+        this.sifre = sifre;
+        this.ad = ad;
+        this.soyad = soyad;
+        this.universite = universite;
+        this.sinifid = sinifid;
+        this.bolumid = bolumid;
+    }
+
+    public Ogrenci(String kullaniciadi, String email, String sifre, String ad, String soyad, String universite, Long sinifid, Long bolumid) {
+        this.kullaniciadi = kullaniciadi;
+        this.email = email;
+        this.sifre = sifre;
+        this.ad = ad;
+        this.soyad = soyad;
+        this.universite = universite;
+        this.sinifid = sinifid;
+        this.bolumid = bolumid;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getKullaniciadi() {
+        return kullaniciadi;
+    }
+
+    public void setKullaniciadi(String kullaniciadi) {
+        this.kullaniciadi = kullaniciadi;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSifre() {
+        return sifre;
+    }
+
+    public void setSifre(String sifre) {
+        this.sifre = sifre;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public void setSoyad(String soyad) {
+        this.soyad = soyad;
+    }
+
+    public String getUniversite() {
+        return universite;
+    }
+
+    public void setUniversite(String universite) {
+        this.universite = universite;
+    }
+
+    public Long getSinifid() {
+        return sinifid;
+    }
+
+    public void setSinifid(Long sinifid) {
+        this.sinifid = sinifid;
+    }
+
+    public Long getBolumid() {
+        return bolumid;
+    }
+
+    public void setBolumid(Long bolumid) {
+        this.bolumid = bolumid;
+    }
+    
+    
+    
 }
