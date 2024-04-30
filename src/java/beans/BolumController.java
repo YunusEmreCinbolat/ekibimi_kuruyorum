aec1877f806dc2faad0b2f95929baa576d77e76d
Fatma Nur Koşum <fatmanurkosum3.@gmail.com>
2024-04-30 15:59:15 +0300

    edit BolumController

--- src/java/beans/BolumController.java
+++ src/java/beans/BolumController.java
@@ -19,17 +19,16 @@ import java.util.List;
  */
 @Named(value = "bolumBean")
 @SessionScoped
-public class BolumController implements Serializable {
+public class BolumController extends BaseController<Bolum ,BolumDAO> implements Serializable {
     
-     private Bolum entity;
-    private List<Bolum> list;
-    private BolumDAO dao;
+     
 
 
     /**
      * Creates a new instance of BolumBean
      */
     public BolumController() {
+       super(Bolum.class ,BolumDAO.class);
     }
     
     public String getTitle(int id){
@@ -55,18 +54,6 @@ public class BolumController implements Serializable {
     }
  
 
-    public Bolum getEntity() {
-      if(this.entity==null){
-            this.entity=new Bolum();
-        }
-        return entity;
-    }
-
-    public String setEntity(Bolum entity) {
-        this.entity = entity;
-        return "/panel/admin/bolum/AdminBolumGuncelle.xhtml?faces-redirect=true";
-    }
-
     public List<Bolum> getList() {
        this.list=this.getDao().readList();
         return list;
@@ -75,18 +62,5 @@ public class BolumController implements Serializable {
     public void setList(List<Bolum> list) {
         this.list = list;
     }
-
-    public BolumDAO getDao() {
-           if (this.dao == null) {
-            this.dao = new BolumDAO();
-        }
-        return dao;
-    }
-
-    public void setDao(BolumDAO dao) {
-        this.dao = dao;
-    }
-    
-    
-    
 }
+    
