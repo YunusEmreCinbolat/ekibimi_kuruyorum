ea86b79473e9d74d10da8c07cde34a1379efe693
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2024-05-10 13:52:16 +0300

    edit project

--- src/java/navigation/NavigationBean.java
+++ src/java/navigation/NavigationBean.java
@@ -6,14 +6,14 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-@Named(value="navigationBean")
+@Named(value = "navigationBean")
 @SessionScoped
 public class NavigationBean implements Serializable {
 
     private Map<String, Boolean> pageStates;
 
     public NavigationBean() {
-       
+
     }
 
     public Map<String, Boolean> getPageStates() {
@@ -21,8 +21,28 @@ public class NavigationBean implements Serializable {
     }
 
     public String goToPage(String page) {
-      pageStates = new HashMap<>();        // Hedef sayfayı aktif yap
-        pageStates.put(page, true);
+        if (pageStates == null) {
+            pageStates = new HashMap<>();
+        } else {
+            // Eğer pageStates zaten oluşturulmuşsa, mevcut zgirdileri kontrol et
+            for (Map.Entry<String, Boolean> entry : pageStates.entrySet()) {
+                String key = entry.getKey();
+                Boolean value = entry.getValue();
+
+                // Eğer sayfa zaten mevcutsa, durumunu güncelleme
+                if (key.contains(page)) {
+                  
+                } 
+                else{
+                      pageStates.put(page, false);
+                    return page;
+                }
+            }
+        }
+
+        // Sayfa daha önce eklenmemişse, yeni bir giriş oluşturma
+        pageStates.put(page, false);
         return page;
     }
+
 }
